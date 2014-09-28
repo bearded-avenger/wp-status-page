a902f642e648b170d27b2c30dedc9319b11dc0a0
Bearded Avenger <email@nickhaskins.com>
2014-09-28 16:32:59 -0500

    remove kruft if status page

--- includes/meta.php
+++ includes/meta.php
@@ -4,6 +4,7 @@ class wpStatusPageMeta {
 
 	function __construct(){
 		add_filter('cmb_meta_boxes', array($this,'meta' ));
+		add_action( 'admin_menu', array($this,'remove_meta' ));
 	}
 
 	/**
@@ -20,6 +21,7 @@ class wpStatusPageMeta {
 			'title' 			=> __('Status Page', 'wp-status-page'),
 			'pages' 			=> array('page'),
 			'context'    		=> 'side',
+			'priority'			=> 'high',
 			'fields' 			=> array(
 				array(
 					'id' 		=> '_wsp_active',
@@ -48,6 +50,18 @@ class wpStatusPageMeta {
 
 		return $meta_boxes;
 
+	}
+
+	function remove_meta(){
+
+		$post_id 		= isset( $_GET['post'] ) ? $_GET['post'] : null;
+		$is_status_page = get_post_meta( $post_id, '_wsp_active', true );
+
+		if ( $is_status_page ):
+ 			remove_meta_box( 'commentstatusdiv' , 'page' , 'normal' ); //removes comments status
+ 			remove_meta_box( 'commentsdiv' , 'page' , 'normal' ); //removes comments
+ 			remove_meta_box( 'authordiv' , 'page' , 'normal' ); //removes author 
+		endif;
 	}
 
 }
