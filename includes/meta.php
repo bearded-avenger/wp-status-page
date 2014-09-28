7f168d7263bb5c369d6eb80238ec6db41dad8e58
Bearded Avenger <email@nickhaskins.com>
2014-09-28 16:23:33 -0500

    setup base opts only run if status page

--- includes/meta.php
+++ includes/meta.php
@@ -13,6 +13,9 @@ class wpStatusPageMeta {
 	*/
 	function meta( array $meta_boxes ) {
 
+		$post_id 		= isset( $_GET['post'] ) ? $_GET['post'] : null;
+		$is_status_page = get_post_meta( $post_id, '_wsp_active', true );
+
 		$meta_boxes[] = array(
 			'title' 			=> __('Status Page', 'wp-status-page'),
 			'pages' 			=> array('page'),
@@ -25,6 +28,23 @@ class wpStatusPageMeta {
 				)
 			)
 		);
+
+		if ( $is_status_page ):
+
+			$meta_boxes[] = array(
+				'title' 	=> __('Status Page Options', 'wp-status-page'),
+				'pages' 	=> array('page'),
+				'fields' 	=> array(
+					array(
+						'id'             => '_wsp_options',
+						'name'           => __('Status Page Options', 'wp-status-page'),
+						'type'           => 'text',
+						'desc'			=> __('Description here','wp-status-page')
+					)
+				)
+			);
+
+		endif;
 
 		return $meta_boxes;
 
