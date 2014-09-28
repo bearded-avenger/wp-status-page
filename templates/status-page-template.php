471f2aa547b82e905de7e68f58e52ea850944c1b
Bearded Avenger <email@nickhaskins.com>
2014-09-28 12:22:33 -0500

    start basic template

--- templates/status-page-template.php
+++ templates/status-page-template.php
@@ -2,6 +2,37 @@
 
 wp_head();
 
-echo 'hi';
+?>
 
-wp_footer();
+<!-- WSP Header -->
+<header class="wsp-header">
+	<div class="wsp-container">
+		header
+	</div>
+</header>
+
+<!-- WSP Main -->
+<main class="wsp-main">
+
+	<div class="wsp-container">
+
+		<div class="wsp-updates">
+			update area
+		</div>
+
+		<div class="wsp-events">
+			history of events area
+		</div>
+
+	</div>
+
+</main>
+
+<!-- WSP Footer -->
+<footer class="wsp-footer">
+	<div class="wsp-container">
+		footer
+	</div>
+</footer>
+
+<?php wp_footer();
